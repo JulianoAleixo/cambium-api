e454296e89741f342f8698a7e69b28411fd60eab
henrique-pizzoni <rikepizzoni@gmail.com>
2026-03-30 22:06:33 -0300

    feat: add route responsible for realizing the conversion between two different currencies

---
+++ app/routes/exchange.py
@@ -0,0 +1,25 @@
+from flask import Blueprint, jsonify, request
+from app.routes.services.frank_service import obter_taxa_de_cambio 
+
+exchange_bp = Blueprint("exchange", __name__)
+
+@exchange_bp.route("/exchange", methods=["GET"])
+def get_exchange():
+    base = request.args.get('base', default='', type=str)
+    dest = request.args.get('dest', default='', type=str)
+    value = request.args.get('value', default=0.0, type=float)
+    
+    try:
+        rate = obter_taxa_de_cambio(base, dest)
+        converted_value = rate * value
+        
+        return jsonify({
+            "moeda_base": base,
+            "moeda_destino": dest,
+            "valor_original": value,
+            "taxa_utilizada": rate,
+            "valor_convertido": converted_value
+        }), 200
+        
+    except Exception as e:
+        return jsonify({"erro": "Falha na conversão", "detalhes": str(e)}), 400
